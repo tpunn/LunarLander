80a88daf54a1a02c5f4819b17fdd6a93632ed60e
Chris DuPuis <cpdupuis@gmail.com>
2017-10-30 21:14:33 -0700

    Such UI. Much Wow.

--- src/com/bitsforabetterworld/lunarlander/Main.java
+++ src/com/bitsforabetterworld/lunarlander/Main.java
@@ -14,11 +14,13 @@ public class Main {
 	}
 	
 	public static void main(String[] args) {
+		final Display display = new Display();
 		SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                Display.createAndShowGUI();
+                display.createAndShowGUI();
             }
 		});
+		System.err.println("Hey there!");
 	}
 
 }
--- src/com/bitsforabetterworld/lunarlander/ui/Display.java
+++ src/com/bitsforabetterworld/lunarlander/ui/Display.java
@@ -1,16 +1,35 @@
 package com.bitsforabetterworld.lunarlander.ui;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
 import javax.swing.*;
 
 
 public class Display {
-   public static void createAndShowGUI() {
+	
+	public void paintScene(Graphics g) {
+		g.setColor(Color.BLUE);
+		g.fillRect(20, 40, 600, 200);
+	}
+	
+   public void createAndShowGUI() {
        //Create and set up the window.
        JFrame frame = new JFrame("HelloWorldSwing");
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-       //Add the ubiquitous "Hello World" label.
-       JLabel label = new JLabel("Hello World");
-       frame.getContentPane().add(label);
+       JPanel panel = new JPanel() {
+    	   // You don't even want to know...	
+    	   private static final long serialVersionUID = -5750610174709683930L;
+
+    	   
+    	   @Override
+    	   public void paintComponent(Graphics g) {
+    		   Display.this.paintScene(g);
+    	   }
+       };
+       panel.setPreferredSize(new Dimension(800, 600));
+       frame.getContentPane().add(panel);
 
        //Display the window.
        frame.pack();

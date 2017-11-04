2f9e00aaf9b667dc91656bf2381cd2fb47e36b7e
Chris DuPuis <cpdupuis@gmail.com>
2017-11-04 15:39:17 -0700

    Add units to constants.

--- src/com/bitsforabetterworld/lunarlander/Constants.java
+++ src/com/bitsforabetterworld/lunarlander/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	public static final double TOP_OF_SCREEN = 1000.0; // meters
 	public static final double WIDTH_OF_SCREEN = 1500.0; // meters
 	
-	public static final double THRUSTER_ACCELERATION = 5.0;
-	public static final double GRAVITY_ACCELERATION = -1.0;
+	public static final double THRUSTER_ACCELERATION = 5.0; // meters/second**2
+	public static final double GRAVITY_ACCELERATION = -1.0; // meters/second**2
 
 }
